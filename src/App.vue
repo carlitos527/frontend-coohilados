52c51b2e93efe2322a59d1564b8b6b15687967c9
Jefferson Becerra <jeffersonbecerra335@gmail.com>
2023-03-31 20:55:24 -0500

    editar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-    <v-app >
-      <Header></Header>
-      <router-view></router-view>
-      <v-content></v-content>
-      <Footer />
-    </v-app>
+  <v-app>
+    <Header></Header>
+    <router-view></router-view>
+    <v-content></v-content>
+    <Footer />
+  </v-app>
 </template>
 <script>
 import Header from "./pages/Header.vue";
@@ -14,12 +14,50 @@ export default {
   components: {
     Header,
     Footer,
+
   },
+  data: () => ({
+    tiempo: 10,
+  }),
+  computed: {
+
+  },
+  methods: {
+    expirarSesion() {
+      let i = setInterval(() => {
+        window.onmousemove = () => {
+          this.tiempo = 900
+        }
+        this.tiempo--
+        console.log(this.tiempo);
+        if (this.tiempo == 300) {
+          this.$swal({
+            icon: "info",
+            title: `Su sesión esta a 5 minutos de cerrar`,
+          });
+        }
+        if (this.tiempo == -1) {
+          this.$store.state.token = undefined
+          this.$router.push("/");
+          localStorage.removeItem("token");
+          localStorage.removeItem("usuario")
+          this.$swal({
+            icon: "info",
+            title: "Su sesión a expirado",
+          });
+          clearInterval(i)
+        }
+      }, 1200)
+    }
+  },
+  created(){
+    console.log("created de app");
+  }
 };
 </script>
 
 <style scoped lang="scss">
- ::v-deep .v-application--wrap {
-    min-height: unset;
-  }
+::v-deep .v-application--wrap {
+  min-height: unset;
+}
 </style>
--- src/pages/Header.vue
+++ src/pages/Header.vue
@@ -1,52 +1,28 @@
 <template>
   <div>
-    <v-app-bar color="#07451E">
-      <v-app-bar-nav-icon
-        class="white--text"
-        @click="drawer = true"
-        v-if="this.$store.state.token !=undefined"
-      ></v-app-bar-nav-icon
-      ><br /><br />
+    <v-app-bar color="#07451E" v-if="navbar != 'oVer'">
+      <v-app-bar-nav-icon class="white--text" @click="drawer = true"></v-app-bar-nav-icon><br /><br />
       <v-row>
-        <v-col cols="1">
-          <v-list-item to="/Home" v-if="this.$store.state.token !=undefined">
+        <v-col cols="1" v-if="$store.state.token != undefined">
+          <v-list-item to="/Home">
             <v-list-item-icon class="white--text mb-2 mt-10">
               <font-awesome-icon icon="fa-solid fa-house" />
             </v-list-item-icon>
-          </v-list-item> </v-col
-        ><br />
+          </v-list-item> </v-col><br />
         <v-col cols="7">
-          <v-toolbar-title class="white--text mb-2 mt-7"
-            ><h3>COOHILADOS</h3>
+          <v-toolbar-title class="white--text mb-2 mt-7">
+            <h3>COOHILADOS</h3>
             <br />
           </v-toolbar-title>
         </v-col>
-        <v-col
-          ><p class="black--text mb-2 mt-4">{{ this.$store.state.usuario.nombre }}</p></v-col
-        >
       </v-row>
 
-      <!-- <v-btn icon>
-        <v-icon class="white--text" @click="buscar()">mdi-magnify</v-icon>
-      </v-btn> -->
-
-      <v-btn
-        class="white--text mx-6"
-        icon
-        v-if="this.$store.state.token !=undefined"
-        @click="cerrarSesion()"
-      >
+      <v-btn class="white--text mx-6" icon v-if="$store.state.token != undefined" @click="cerrarSesion()">
         <v-icon>mdi-account-off</v-icon>
       </v-btn>
       <v-menu left bottom> </v-menu>
       <div class="mx-6">
-        <v-btn
-          color="orange darken-2"
-          fab
-          dark
-          to="/"
-          v-if="this.$store.state.token ===undefined"
-        >
+        <v-btn color="orange darken-2" fab dark to="/" v-if="$store.state.token === undefined">
           <v-icon>mdi-account-circle</v-icon>
         </v-btn>
       </div>
@@ -58,52 +34,41 @@
 
     <v-navigation-drawer
       src="https://eurofique.info/wp-content/gallery/visita-virtual-expo-fique-historia-y-futuro/07-saco-de-fique.jpg"
-      v-model="drawer"
-      absolute
-      temporary
-      v-if="$store.state.token !=undefined"
-    >
-      <v-img
-        src="https://coohilados.com.co/carga/upload/images/QUIENESSOMOS1.jpg"
-      >
+      v-model="drawer" absolute temporary v-if="$store.state.token != undefined">
+      <v-img src="https://coohilados.com.co/carga/upload/images/QUIENESSOMOS1.jpg">
         <v-list nav dense>
-          <v-list-item-group
-            color="orange darken-4"
-            v-model="group"
-            active-class="purple lighten-2"
-          >
+          <v-list-item-group color="orange darken-4" v-model="group" active-class="purple lighten-2">
             <h1>INICIO</h1>
-
-            <v-list-group
-              color="green accent-4"
-              :value="true"
-              no-action
-              sub-group
-            >
-              <v-list-item color="red accent-3" to="Gerencia" v-if="$store.state.usuario.rol=='Visualizador'">
+            <v-list-group color="green accent-4" :value="true" no-action sub-group>
+              <v-list-item color="red accent-3" to="Gerencia" v-if="$store.state.usuario.rol == 'Visualizador'">
                 <h4>CONSEJO O GERENCIA</h4>
               </v-list-item>
-
-               <v-list-item color="green accent-4" to="TH" v-if="$store.state.usuario.rol=='Editor de Datos'">
+              <v-list-item color="green accent-4" to="TH" v-if="$store.state.usuario.rol == 'Editor de Datos'">
                 <h3>TALENTO HUMANO</h3>
               </v-list-item>
-
-              <v-list-item color="red accent-3" to="Sistemas" v-if="$store.state.usuario.rol=='Administrador'">
+              <v-list-item color="red accent-3" to="Sistemas" v-if="$store.state.usuario.rol == 'Administrador'">
                 <h3>SISTEMAS</h3>
               </v-list-item>
-
-              <v-list-item color="red accent-3" to="Sst" v-if="$store.state.usuario.rol=='Actualizador'">
+              <v-list-item color="red accent-3" to="Sst" v-if="$store.state.usuario.rol == 'Actualizador'">
                 <h3>SST</h3>
               </v-list-item>
-            
-
-
             </v-list-group>
-            
-             
           </v-list-item-group>
         </v-list>
       </v-img>
+      <v-row>
+        <v-col class="text-center">
+          <div style="background-color: rgba(1,1,1,0.4);">
+            <h1>
+              {{ `${$store.state.usuario.nombre}` }}
+            </h1>
+            <h3>
+              {{ `Área: ${$store.state.usuario.area}` }} <br>
+              {{ `Rol: ${$store.state.usuario.rol}` }}
+            </h3>
+          </div>
+        </v-col>
+      </v-row>
     </v-navigation-drawer>
   </div>
 </template>
@@ -115,29 +80,62 @@ export default {
     group: null,
     cerrar: "",
     persona: "",
+    navbar: ""
   }),
   methods: {
     cerrarSesion() {
-      this.$store.state.token=undefined
-
+      this.$store.state.token == undefined
       this.$router.push("/");
-      console.log("cerrarSesion");
       localStorage.removeItem("token");
-      localStorage.removeItem("usuario")
-
+      localStorage.removeItem("usuario");
+      localStorage.setItem('nav', 'noVer')
+      /* localStorage.clear(); */
       this.$swal({
-            icon: "success",
-            title: "Usted cerro sesión exitosamente. 👋",
-          });
+        icon: "success",
+        title: "Usted cerro sesión exitosamente. 👋",
+      });
     },
     traerUsuario() {
       this.persona = this.$store.state.usuario;
-      console.log(this.persona);
-      console.log(this.$store.state.usuario);
     },
+    expirarSesion() {
+      let i = setInterval(() => {
+        window.onmousemove = () => {
+          this.tiempo = 900
+        }
+        this.tiempo--
+        console.log(this.tiempo);
+        if (this.tiempo == 450) {
+          this.$swal({
+            icon: "info",
+            title: `Su sesión esta a 5 segundos de cerrar`,
+          });
+        }
+        if (this.tiempo == -1) {
+          this.$store.state.token = undefined
+          this.$router.push("/");
+          localStorage.removeItem("token");
+          localStorage.removeItem("usuario")
+          this.$swal({
+            icon: "info",
+            title: "Su sesión a expirado",
+          });
+          clearInterval(i)
+        }
+      }, 1200)
+    },
+    traer() {
+      this.navbar = localStorage.getItem('nav');
+      console.log(this.navbar);
+    }
   },
   created() {
     this.traerUsuario();
+    this.traer();
+    console.log("created de header");
   },
+  mounted(){
+    console.log("mounted de header");
+  }
 };
 </script>
