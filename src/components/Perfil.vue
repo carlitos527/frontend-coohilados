6cfcdd07c17357a7af65e5e92e88f452101cb76c
Jefferson Becerra <jeffersonbecerra335@gmail.com>
2023-04-05 10:42:04 -0500

    arreglo del perfil

--- src/components/Perfil.vue
+++ src/components/Perfil.vue
@@ -1,58 +1,40 @@
 <template>
-  <v-container
-    ><br />
+  <v-container><br />
 
     <v-row>
       <v-col colored-border type="warning">
         <v-card class="mx-auto" max-width="10000" tile>
           <template slot="progress">
-            <v-progress-linear
-              color="deep-purple"
-              height="10"
-              indeterminate
-            ></v-progress-linear>
+            <v-progress-linear color="deep-purple" height="10" indeterminate></v-progress-linear>
           </template>
           <v-row>
             <v-col class="yellow accent-1">
               <v-row align="end" class="fill-height">
                 <v-col align-self="start" class="pa-0" cols="12">
                   <v-avatar class="profile" color="grey" size="164" tile>
-                    <v-img
-                      src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"
-                    ></v-img>
+                    <v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"></v-img>
                   </v-avatar>
-
-                  <v-row align="center" class="mx-0">
-                    <v-col cols="12" sm="6" md="4">
-                      <p
-                        label="Estado"
-                        v-if="this.$store.state.datos.estado == 1"
-                      >
-                        Estado: Activo
-                      </p>
-                      <p
-                        label="Estado"
-                        v-if="this.$store.state.datos.estado == 2"
-                      >
-                        Estado: Inactivo
-                      </p>
-                      <p
-                        label="Estado"
-                        v-if="this.$store.state.datos.estado == 3"
-                      >
-                        Estado: De vacaciones
-                      </p>
-                    </v-col>
-                  </v-row>
 
                   <v-card-text>
                     <v-row align="center" class="mx-0">
                       <v-col cols="12" sm="6" md="4">
-                        <p label="Estado">
-                          {{ `Área: ${$store.state.usuario.area}` }} <br />
-                          {{ `Rol: ${$store.state.usuario.rol}` }} <br />
-                          {{ `Estado: ${$store.state.usuario.estado}` }}
+                        <h3 class="mb-0">{{ `Área: ${$store.state.usuario.area}` }}</h3>
+                        <h3 class="mb-0">{{ `Rol: ${$store.state.usuario.rol}` }}</h3>
+                        <h3 v-if="$store.state.usuario.estado == 1"> Estado:
+                          <p label="Estado" style="color: green" >
+                            Activo
+                          </p>
+                        </h3>
+                        <h3 v-if="$store.state.usuario.estado == 2"> Estado:
+                          <p label="Estado" style="color:red" >
+                          Inactivo
                         </p>
+                        </h3>
+                        <h3 v-if="$store.state.usuario.estado == 3">Estado:
+                          <p label="Estado" style="color: orange" >
+                          Estado: De vacaciones
+                        </p>
+                        </h3>
                       </v-col>
                       <v-card-title justify-right>
                         <v-card-title>{{
@@ -63,58 +45,37 @@
                   </v-card-text>
 
                   <v-divider class="mx-4"></v-divider>
-                  
+
                   <!-- editar usuario -->
                   <v-card-text>
                     <template>
                       <v-container>
                         <v-row>
                           <v-col cols="12" sm="6" md="6">
-                            <v-text-field
-                              v-model="detalleUsuario.documento"
-                              :rules="documentoRules"
-                              label="Documento"
-                            ></v-text-field>
+                            <v-text-field v-model="detalleUsuario.documento" :rules="documentoRules"
+                              label="Documento"></v-text-field>
                           </v-col>
 
                           <v-col cols="12" sm="6" md="6">
-                            <v-text-field
-                              v-model="detalleUsuario.nombre"
-                              :rules="nombreRules"
-                              label="Nombres"
-                            ></v-text-field>
+                            <v-text-field v-model="detalleUsuario.nombre" :rules="nombreRules"
+                              label="Nombres"></v-text-field>
                           </v-col>
 
                           <v-col cols="12" sm="6" md="6">
-                            <v-text-field
-                              v-model="detalleUsuario.email"
-                              label="E-mail"
-                              :rules="emailRules"
-                            ></v-text-field>
+                            <v-text-field v-model="detalleUsuario.email" label="E-mail"
+                              :rules="emailRules"></v-text-field>
                           </v-col>
                           <v-col cols="12" sm="6" md="6">
-                            <v-text-field
-                              v-model="detalleUsuario.password"
-                              label="Password"
-                              :rules="passwordlRules"
-                            ></v-text-field>
+                            <v-text-field v-model="detalleUsuario.password" label="Password"
+                              :rules="passwordlRules"></v-text-field>
                           </v-col>
                         </v-row>
                         <v-card-actions>
-                          <v-btn
-                            color="Black lighten-2"
-                            text
-                            to="/Home"
-                            class="mb-2 green accent-3"
-                          >
+                          <v-btn color="Black lighten-2" text to="/Home" class="mb-2 orange darken-4">
                             Regresar
                           </v-btn>
-                          <v-btn
-                            class="mb-2 red darken-4"
-                            color="Black lighten-2"
-                            text
-                            @click="editarItem(detalleUsuario._id)"
-                          >
+                          <v-btn class="mb-2  green accent-3" color="Black lighten-2" text
+                            @click="editarItem(detalleUsuario._id)">
                             Guardar
                           </v-btn>
                         </v-card-actions>
@@ -141,7 +102,6 @@ export default {
     loading: false,
     dialog: false,
     dialogEdit: false,
-
     valid: true,
     nombre: "",
     nombreRules: [
@@ -149,7 +109,6 @@ export default {
       (n) =>
         (n && n.length <= 50) || " EL Nombre solo puede tener 50 caracteres",
     ],
-
     vali2: true,
     documento: "",
     documentoRules: [
@@ -157,7 +116,6 @@ export default {
       (d) =>
         (d && d.length <= 17) || " Cedula No puede tener menos de 6 caracteres",
     ],
-
     vali5: true,
     email: "",
     emailRules: [
@@ -174,9 +132,7 @@ export default {
         (p && p.length <= 20) ||
         " La contraseña  no puede tener menos de 8 caracteres",
     ],
-
     usuarios: [],
-
     detalleUsuario: {
       documento: "",
       nombre: "",
@@ -187,12 +143,11 @@ export default {
   }),
   methods: {
     traerUsuario() {
-      this.id = this.$store.state.datos._id;
+      this.id = this.$store.state.usuario._id;
       this.detalleUsuario = {
-        documento: this.$store.state.datos.documento,
-        nombre: this.$store.state.datos.nombre,
-        email: this.$store.state.datos.email,
-        password: this.$store.state.datos.password,
+        documento: this.$store.state.usuario.documento,
+        nombre: this.$store.state.usuario.nombre,
+        email: this.$store.state.usuario.email,
       };
     },
     editarItem() {
@@ -226,6 +181,9 @@ export default {
           });
         });
     },
+    estado() {
+
+    }
   },
 
   created() {
