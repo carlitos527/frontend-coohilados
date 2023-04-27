b8ed0f39b5a7f42da173c9ca3a55b0490e181018
carlitos527 <carlos_inpact_527@hotmail.es>
2023-04-27 11:35:29 -0500

    error letra

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,7 +15,7 @@
           <h3 v-if="user.rol == 'Actualizador'"> "Usted visualizar todos los trabajadores y anexar anotaciones"
             y imprimir reportes</h3>
           <h3 v-if="user.rol == 'Editor de Datos'"> Usted puede editar todos los Trabajadores, imprimir reportes,
-            vusualizar y editar </h3>
+            visualizar y editar </h3>
           <h3 v-if="user.rol == 'Visualizador'"> Usted puede Visualizar todos los trabajadores e imprimir reportes </h3>
         </section>
       </v-col>
